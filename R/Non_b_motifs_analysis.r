e78aad04db39fc311d9715d8d483e682714f1bfe
Juliana Edelvacy <152703729+JulianaEdelvacy@users.noreply.github.com>
2025-07-21 14:53:22 -0300

    Update Non_b_motifs_analysis.r

--- R/Non_b_motifs_analysis.r
+++ R/Non_b_motifs_analysis.r
@@ -1,1 +1,11 @@
 # Result analysis from Non Beta motifs search tool
+
+#Loading needed t
+library(data.table)
+
+#Couting and plotting the repetitions of direct repeats motifs.
+# Loading Direct repeat result file
+DR <- read.csv("687514b02f75d_DR.tsv", sep = "\t", stringsAsFactors = FALSE, header = TRUE)
+
+
+#
