c30d2ccccdb94d174e5aa6349a3342d97b4bc292
Juliana Edelvacy <152703729+JulianaEdelvacy@users.noreply.github.com>
2025-07-21 14:55:42 -0300

    Update Non_b_motifs_analysis.r

--- R/Non_b_motifs_analysis.r
+++ R/Non_b_motifs_analysis.r
@@ -1,11 +1,12 @@
 # Result analysis from Non Beta motifs search tool
 
-#Loading needed t
+# Loading needed libraries, if you don't have any of these libraries just use the command install.packages(your_library).
 library(data.table)
 
 #Couting and plotting the repetitions of direct repeats motifs.
 # Loading Direct repeat result file
 DR <- read.csv("687514b02f75d_DR.tsv", sep = "\t", stringsAsFactors = FALSE, header = TRUE)
 
-
-#
+# Load your gene conversion results from Brepconvert
+data1 <- read.csv("final_filtered_result.csv", sep = "\t", stringsAsFactors = FALSE, header = TRUE)
+df_filtered_selecionado <- as.data.table(df_filtered_selecionado)
